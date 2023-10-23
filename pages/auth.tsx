d532cdff26e7acad9498cafd8928a15e5cd747c4
yosuke37 <yosukenji@gmail.com>
2023-10-23 01:26:21 -0400

    Added Confirm Password Component - UI

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,9 +1,11 @@
 import Input from "@/components/Input";
 import EmailIcon from '../public/sms.svg';
+import LockIcon from '../public/lock.svg';
 import { useState } from "react";
 
 const Auth = () => {
   const [email, setEmail] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
   return (
     <>
@@ -15,6 +17,15 @@ const Auth = () => {
         id="email"
         type="email"
         value={email}
+      />
+      <Input
+        image={LockIcon}
+        alt="Lock Icon"
+        label="Confirm Password"
+        onChange={(ev: any) => setConfirmPassword(ev.target.value)}
+        id="confirmPassword"
+        type="confirmPassword"
+        value={confirmPassword}
       />
     </>
   );
