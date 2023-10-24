29066c45eff1ab5316b645875efe0bf2d00cc815
Matthew Barrera <mub6@njit.edu>
2023-10-23 23:25:14 -0400

    password component added

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,9 +1,12 @@
 import Input from "@/components/Input";
 import EmailIcon from '../public/sms.svg';
+import LockIcon from '../public/lock.svg';
 import { useState } from "react";
 
 const Auth = () => {
   const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
   return (
     <>
@@ -15,6 +18,24 @@ const Auth = () => {
         id="email"
         type="email"
         value={email}
+      />
+      <Input
+        image={LockIcon}
+        alt='Lock Icon'
+        label="Password"
+        onChange={(ev: any) => setPassword(ev.target.value)}
+        id='password'
+        type='password'
+        value={password}
+      />
+      <Input
+        image={LockIcon}
+        alt="Lock Icon"
+        label="Confirm Password"
+        onChange={(ev: any) => setConfirmPassword(ev.target.value)}
+        id="confirmPassword"
+        type="confirmPassword"
+        value={confirmPassword}
       />
     </>
   );
