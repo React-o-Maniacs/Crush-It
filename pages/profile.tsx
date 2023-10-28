46827cf1f8e10bff4679f7009a4213d6ee4a0f8e
Akram Abounaja <aa232@njit.edu>
2023-10-28 01:36:30 -0400

    finished UserInfo Component - UI

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,13 +1,55 @@
-import SideBanner from '@/components/SideBanner';
-import ProfileAvatar from '../components/ProfileAvatar';
+import SideBanner from "@/components/SideBanner";
+import ProfileAvatar from "../components/ProfileAvatar";
+import React, { useState } from "react";
+import UserImage from "../public/images/profile.svg";
+import Input from "../components/Input";
 
 const Profile = () => {
-    return (
-        <div>
-        <ProfileAvatar name="John Doe" />
-        <SideBanner/>
-        </div>
-    )
+  const [FirstName, setFirstName] = useState("");
+  const [LastName, setLastName] = useState("");
 
-    };
-    export default Profile;
+  return (
+    <div className="flex min-h-screen">
+      <SideBanner />
+      <div className="flex-1 flex flex-col">
+        <div className="bg-white p-4 shadow-md">
+          <div className="flex justify-between items-center">
+            <h1>Profile</h1>
+            <ProfileAvatar name="John Doe" />
+          </div>
+        </div>
+
+        <h2 className="text-2xl font-semibold mt-9 mx-4">User Info</h2>
+
+        <div className="bg-white p-4 shadow-md my-4 mx-4 rounded-lg">
+          <div className="flex space-x-4">
+            <div className="flex-1">
+              <Input
+                image={UserImage}
+                alt="Image Icon"
+                label="First Name"
+                onChange={(e: any) => setFirstName(e.target.value)}
+                id="firstName"
+                type="text"
+                value={FirstName}
+              />
+            </div>
+            <div className="flex-1">
+              <Input
+                image={UserImage}
+                alt="Image Icon"
+                label="Last Name"
+                onChange={(e: any) => setLastName(e.target.value)}
+                id="lastName"
+                type="text"
+                value={LastName}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
