0abf61b097f444aa40f85a3308f1fec73388ef75
Kareem Hegazy <kthegazy2002@gmail.com>
2023-10-27 22:55:08 -0400

    Signup form container done

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,6 +1,6 @@
 import Input from "@/components/Input";
-import EmailIcon from '../public/sms.svg';
-import LockIcon from '../public/lock.svg';
+import EmailIcon from '../public/images/sms.svg';
+import LockIcon from '../public/images/lock.svg';
 import { useState } from "react";
 
 const Auth = () => {
@@ -12,11 +12,41 @@ const Auth = () => {
     <div className="flex flex-1">
       <div className="flex flex-1 h-screen max-w-[65%] rounded-r-[10px] bg-crush-it-black bg-cover items-center z-0">
         <div className='flex flex-1 flex-col items-center'>
+          <h1 className="flex font-normal text-[90px] text-white">Crush It</h1>
+          <div className='bg-green-500 w-[200px] h-[200px]'>
 
+          </div>
         </div>
         <div className="flex h-[80%] w-[60%] z-2 mr-[-50%]">
           <div className="flex bg-white rounded-[10px] flex-1 flex-col p-[10%] space-y-[5%]">
-            
+            <h1 className="flex font-bold text-[60px] text-crush-it-black">Sign Up</h1>
+            <Input
+              image={EmailIcon}
+              alt="Email Icon"
+              label="Email/username"
+              onChange={(ev: any) => setEmail(ev.target.value)}
+              id="email"
+              type="email"
+              value={email}
+            />
+            <Input
+              image={LockIcon}
+              alt="Lock Icon"
+              label="Password"
+              onChange={(ev: any) => setPassword(ev.target.value)}
+              id="password"
+              type="password"
+              value={password}
+            />
+            <Input
+              image={LockIcon}
+              alt="Lock Icon"
+              label="Confirm Password"
+              onChange={(ev: any) => setConfirmPassword(ev.target.value)}
+              id="confirmPassword"
+              type="password"
+              value={confirmPassword}
+            />
           </div>
         </div>
       </div> 
