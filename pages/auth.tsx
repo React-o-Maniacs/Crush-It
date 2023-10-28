a859f4cccc3ddc19b9f6f32753524e84cb92aa50
yosuke37 <yosukenji@gmail.com>
2023-10-27 23:15:22 -0400

    Crush it logo done

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,6 +1,8 @@
 import Input from "@/components/Input";
 import EmailIcon from '../public/images/sms.svg';
 import LockIcon from '../public/images/lock.svg';
+import Image from "next/image";
+import CrushItLogo from '../public/images/crush-it-logo.svg';
 import { useState } from "react";
 
 const Auth = () => {
@@ -13,9 +15,7 @@ const Auth = () => {
       <div className="flex flex-1 h-screen max-w-[65%] rounded-r-[10px] bg-crush-it-black bg-cover items-center z-0">
         <div className='flex flex-1 flex-col items-center'>
           <h1 className="flex font-normal text-[90px] text-white">Crush It</h1>
-          <div className='bg-green-500 w-[200px] h-[200px]'>
-
-          </div>
+          <Image src={CrushItLogo} alt='Crush It Logo' width={600} height={600}/>
         </div>
         <div className="flex h-[80%] w-[60%] z-2 mr-[-50%]">
           <div className="flex bg-white rounded-[10px] flex-1 flex-col p-[10%] space-y-[5%]">
