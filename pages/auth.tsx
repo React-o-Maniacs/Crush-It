572e95fc4e3f704262388ebce328839c1d344e2d
Kareem Hegazy <kthegazy2002@gmail.com>
2023-10-28 14:36:41 -0400

    sign Up Button

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -14,10 +14,10 @@ const Auth = () => {
     <div className="flex flex-1">
       <div className="flex flex-1 h-screen max-w-[65%] rounded-r-[10px] bg-crush-it-black bg-cover items-center z-0">
         <div className='flex flex-1 flex-col items-center'>
-          <h1 className="flex font-normal text-[90px] text-white">Crush It</h1>
+          <h1 className="flex font-normal text-[90px] text-white ">Crush It</h1>
           <Image src={CrushItLogo} alt='Crush It Logo' width={600} height={600}/>
         </div>
-        <div className="flex h-[80%] w-[60%] z-2 mr-[-50%]">
+        <div className="flex h-[80%] w-[60%] z-2 mr-[-50%] shadow-2xl">
           <div className="flex bg-white rounded-[10px] flex-1 flex-col p-[10%] space-y-[5%]">
             <h1 className="flex font-bold text-[60px] text-crush-it-black">Sign Up</h1>
             <Input
@@ -50,6 +50,9 @@ const Auth = () => {
               type="password"
               value={confirmPassword}
             />
+            <div className="flex justify-center">
+              <button className="h-[60px] w-[270px] bg-gradient-to-b from-crush-it-blue to-crush-it-blue-g hover:from-sky-500 hover:to-blue-600 text-white text-[18px] font-bold py-2 px-4 rounded-[16px] ">Sign Up</button>
+            </div>
           </div>
         </div>
       </div> 
