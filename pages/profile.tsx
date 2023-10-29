e31df90bda331e9b6659d15024aabe09025063d6
Akram Abounaja <aa232@njit.edu>
2023-10-28 22:01:12 -0400

    Finished Change Pasword Component UI

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -3,10 +3,14 @@ import ProfileAvatar from "../components/ProfileAvatar";
 import React, { useState } from "react";
 import UserImage from "../public/images/profile.svg";
 import Input from "../components/Input";
+import LockImage from '../public/images/lock.svg';
 
 const Profile = () => {
   const [FirstName, setFirstName] = useState("");
   const [LastName, setLastName] = useState("");
+  const [currentPassword, setCurrentPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
   return (
     <div className="flex min-h-screen">
@@ -32,6 +36,7 @@ const Profile = () => {
                 id="firstName"
                 type="text"
                 value={FirstName}
+                inputFieldClassName="flex-1"
               />
             </div>
             <div className="flex-1">
@@ -43,6 +48,56 @@ const Profile = () => {
                 id="lastName"
                 type="text"
                 value={LastName}
+                inputFieldClassName="flex-1"
+              />
+            </div>
+          </div>
+        </div>
+
+        <h2 className="text-2xl font-semibold mt-9 mx-4">Change Password</h2>
+
+        <div className="bg-white p-4 shadow-md my-4 mx-4 rounded-lg">
+          <div className="flex flex-row space-x-4">
+            <div className="flex-1">
+              <Input
+                image={LockImage}
+                label="Current Password"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setCurrentPassword(e.target.value)
+                }
+                id="currentPassword"
+                type="password"
+                value={currentPassword}
+                alt="Current Password"
+                inputFieldClassName="flex-1"
+              />
+            </div>
+            <div className="flex-1">
+              <Input
+                image={LockImage}
+                label="New Password"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setNewPassword(e.target.value)
+                }
+                id="newPassword"
+                type="password"
+                value={newPassword}
+                alt="New Password"
+                inputFieldClassName="flex-1"
+              />
+            </div>
+            <div className="flex-1">
+              <Input
+                image={LockImage}
+                label="Confirm New Password"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setConfirmPassword(e.target.value)
+                }
+                id="confirmPassword"
+                type="password"
+                value={confirmPassword}
+                alt="Confirm New Password"
+                inputFieldClassName="flex-1"
               />
             </div>
           </div>
