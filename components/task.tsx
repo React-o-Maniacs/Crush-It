b2ba26ffb3b4de064cd66d658bbabe728c970540
Akram Abounaja <aa232@njit.edu>
2023-11-07 20:01:38 -0500

    task drop down Icon and Functionality Completed

--- components/task.tsx
+++ components/task.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Image, { StaticImageData } from 'next/image';
-
+import rightArrow from "../public/images/rightarrow.svg";
+import downArrow from "../public/images/downarrow.svg";
 
 
 
@@ -15,15 +16,30 @@ interface TaskProps {
 }
 
 const Task: React.FC<TaskProps> = ({ task }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
 
-
+  const handleToggleExpand = () => {
+    setIsExpanded(!isExpanded);
+  };
 
   return (
     <div className="bg-white p-2 my-2 rounded">
       <div className="flex items-center">
         <span className={`flex-1`}>{task.title}</span>
-
+        <button onClick={handleToggleExpand} className="p-2 rounded">
+          <Image
+            src={isExpanded ? downArrow : rightArrow}
+            alt="Expand"
+          />
+        </button>
       </div>
+      {/* Expandable area for more task info */}
+      {isExpanded && (
+        <div className="mt-4 p-4 rounded">
+          {/* Replace this with the actual task details */}
+          <div>Task details go here.</div>
+        </div>
+      )}     
     </div>
   );
 };
