be1193242763460f12a0fe9003ffec6b5fc4083d
Kareem Hegazy <kthegazy2002@gmail.com>
2023-11-09 17:28:52 -0500

    Added first test

--- __tests__/Profile.test.tsx
+++ __tests__/Profile.test.tsx
@@ -3,11 +3,35 @@
  */
 import { render, screen } from '@testing-library/react'
 import Profile from '@/pages/profile'
+import '@testing-library/jest-dom';
 
-it('should have Profile text', () => {
-    render(<Profile />) // ARRANGE
+jest.mock('next/router', () => require('next-router-mock'));
+describe('Profile Page UI', () => {
+    it('Should have profile section title UI elements', () => {
+        // Arrange
+        render(<Profile />)
 
-    const myElem = screen.getByText('Profile') // ACT
+        // Actions
+        const profileTitle = screen.getByText('Profile')
+        const userInfoTitle = screen.getByText('User Info')
+        const changePasswordTitle = screen.getByText('Change Password')
+        const pomodoroTimerTitle = screen.getByText('Pomodoro Timer (Minutes)')
 
-    expect(myElem).toBeInTheDocument() // ASSERT
+        // Assertions
+        expect(profileTitle).toBeInTheDocument()
+        expect(userInfoTitle).toBeInTheDocument()
+        expect(changePasswordTitle).toBeInTheDocument()
+        expect(pomodoroTimerTitle).toBeInTheDocument()
+    })
+    it('Should have profile section title UI elements', () => {
+        // Arrange
+        render(<Profile />)
+
+        // Actions
+        const firstNameInputComponent = screen.getByTestId('Input')
+
+        // Assertions
+        expect(firstNameInputComponent).toBeInTheDocument()
+
+    })
 })
