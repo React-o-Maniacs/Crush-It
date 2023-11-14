b0b1b116a3aed7e2be45438e63974ca00e6836fa
Kareem Hegazy <kthegazy2002@gmail.com>
2023-11-13 20:53:53 -0500

    Code Cleanup

--- pages/api/updateTaskStatus.ts
+++ pages/api/updateTaskStatus.ts
@@ -3,7 +3,6 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import prismadb from '@/lib/prismadb';
 import serverAuth from '@/lib/serverAuth';
 
-
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'PATCH') {
     return res.status(405).end();
